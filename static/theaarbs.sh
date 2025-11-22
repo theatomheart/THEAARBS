9fd63c736d43852b0483ccaf45cd3b8e060e8120
theatomheart <theatomheart@pm.me>
2025-11-22 15:19:19 -0600

    installer changes

--- static/theaarbs.sh
+++ static/theaarbs.sh
@@ -6,8 +6,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/static/progs.csv"
+dotfilesrepo="https://github.com/theatomheart/archconfig.git"
+progsfile="https://raw.githubusercontent.com/theatomheart/THEAARBS/master/static/progs.csv"
 aurhelper="yay"
 repobranch="master"
 export TERM=ansi
